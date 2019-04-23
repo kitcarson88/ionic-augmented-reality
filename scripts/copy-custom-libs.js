7ffdfc806b698b6d980bc79289432757ccfcbe36
Simone Colazzo <colsimo88@gmail.com>
2019-04-23 20:24:25 +0200

    Style on working

--- scripts/copy-custom-libs.js
+++ scripts/copy-custom-libs.js
@@ -40,13 +40,22 @@ function copyFolderRecursiveSync( source, target ) {
 
 module.exports = function(ctx) {
   console.log("Copy custom libs and fonts");
+  console.log("\tCreate fonts directory");
   //Create fonts directory if it not exists
   if (!fs.existsSync("./src/assets/fonts"))
     fs.mkdirSync("./src/assets/fonts");
 
   //Copy roboto fonts
+  console.log("\tCopy roboto font");
   if (!fs.existsSync("./src/assets/fonts/roboto"))
     fs.mkdirSync("./src/assets/fonts/roboto");
   copyFolderRecursiveSync("./node_modules/roboto-fontface/css", "./src/assets/fonts/roboto");
   copyFolderRecursiveSync("./node_modules/roboto-fontface/fonts", "./src/assets/fonts/roboto");
+
+  //Copy josefin fonts
+  console.log("\tCopy josefin font");
+  if (!fs.existsSync("./src/assets/fonts/josefin"))
+    fs.mkdirSync("./src/assets/fonts/josefin");
+  copyFolderRecursiveSync("./node_modules/typeface-josefin-sans/files", "./src/assets/fonts/josefin");
+  copyFileSync("./node_modules/typeface-josefin-sans/index.css", "./src/assets/fonts/josefin");
 };
