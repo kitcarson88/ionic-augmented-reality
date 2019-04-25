b4253e4c22a29f8661d589509ede35ffcf3ef98d
Simone Colazzo <colsimo88@gmail.com>
2019-04-25 16:37:33 +0200

    About me page

--- src/pages/about-author/about-author.module.ts
+++ src/pages/about-author/about-author.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
 import { IonicModule } from '@ionic/angular';
 
-import { AboutAuthorPage } from './about-author.page';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { createTranslateLoader } from '../../app/app.module';
+import { HttpClient } from '@angular/common/http';
 
+import { AboutAuthorPage } from './about-author.page';
 import { MenuOverlayModule } from '../../components/menu-overlay/menu-overlay.module';
 
 const routes: Routes = [
@@ -22,6 +24,14 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     MenuOverlayModule,
+    TranslateModule.forChild({
+      loader: {
+           provide: TranslateLoader,
+           useFactory: (createTranslateLoader),
+           deps: [ HttpClient ]
+         }
+      }
+    ),
     RouterModule.forChild(routes)
   ],
   declarations: [AboutAuthorPage]
